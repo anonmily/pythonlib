fef13f4023100d5823fcd69a31a4e35000e48af1
Michelle Liu <michelle@michelleliu.io>
2016-07-06 15:14:23 -0700

    added in install requires setup.py

--- setup.py
+++ setup.py
@@ -12,29 +12,20 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='michellepylib',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='1.0.0',
-
     description="Michelle's Python toolkit",
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/anonmily/pythonlib',
-
-    # Author details
     author='Michelle Liu',
     author_email='michelle@michelleliu.io',
-
-    # Choose your license
     license='MIT',
-
-    # What does your project relate to?
     keywords='files loggin message email mongodb aws sqs diff',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+      'pymongo',
+      'bson',
+      'boto3',
+      'smtplib',
+      'yaml'
+  ]
 )
